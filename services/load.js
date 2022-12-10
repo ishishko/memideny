af5feb3ee5874fb55b59be044e1fd99e57338d4c
Ishishko <shishkoignacio@gmail.com>
2022-12-09 22:28:33 -0300

    Preparado para conectar a firebase

--- services/load.js
+++ services/load.js
@@ -1,26 +1,20 @@
 spinCarga();
 
-function spinCarga() {
+export function spinCarga() {
   const body = document.querySelector("body");
   const divSpin = document.createElement("div");
-  divSpin.classList.add("sk-circle");
-  divSpin.innerHTML = `<div class="sk-circle1 sk-child"></div>
-                        <div class="sk-circle2 sk-child"></div>
-                        <div class="sk-circle3 sk-child"></div>
-                        <div class="sk-circle4 sk-child"></div>
-                        <div class="sk-circle5 sk-child"></div>
-                        <div class="sk-circle6 sk-child"></div>
-                        <div class="sk-circle7 sk-child"></div>
-                        <div class="sk-circle8 sk-child"></div>
-                        <div class="sk-circle9 sk-child"></div>
-                        <div class="sk-circle10 sk-child"></div>
-                        <div class="sk-circle11 sk-child"></div>
-                        <div class="sk-circle12 sk-child"></div>`;
+  divSpin.classList.add("loader-3");
+  divSpin.innerHTML = `
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>`;
   body.appendChild(divSpin);
 }
 
 export default function cargaTerminada() {
-  const spinner = document.querySelector(".sk-circle");
+  const spinner = document.querySelector(".loader-3");
   const header = document.querySelector("header");
   const main = document.querySelector("main");
   const footer = document.querySelector("footer");
@@ -29,5 +23,5 @@ export default function cargaTerminada() {
     main.classList.add("opacity");
     footer.classList.add("opacity");
     spinner.remove();
-  }, 1000);
+  }, 2500);
 }
